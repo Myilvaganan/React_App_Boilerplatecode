74694050e793b230b133c694c3dd948a3cefd934
Myil <myilsmp@gmail.com>
2021-08-14 23:14:16 +0530

    Added for Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hi, This is a React App set-up using webpack and babel without using
+      create-react-app.
+      <blockquote>
+        <em>By Myilvaganan</em>
+      </blockquote>
+    </div>
+  );
+};
+
+export default App;
